cba6c6d2a0de3d0a72784748f2dfa28f48695d5b
TokyoLols <133028307+ReallyDev1@users.noreply.github.com>
2025-02-15 07:16:43 +0000

    1

--- test.lua
+++ test.lua
@@ -1,2 +1,2 @@
 print("auto executed")
-queue_on_teleport(loadstring(game:HttpGet("https://github.com/ReallyDev1/testing.git"))())
+queue_on_teleport(loadstring(game:HttpGet("https://github.com/ReallyDev1/testing.git")))
